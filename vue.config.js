d79060fb80f3b5be94b2d5d8addd9f54b7434bc7
DuanHongFei <1715106673@qq.com>
2021-05-25 18:04:33 +0800

    完成代理和推荐界面

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const registerRouter = require('./backend/router')
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量与mixin
+        prependData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  },
+  devServer: {
+    before(app) {
+      registerRouter(app)
+    }
+  }
+}
